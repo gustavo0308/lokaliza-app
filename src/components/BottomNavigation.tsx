55c6e59c15ac234308392611b0e15389f9a5a797
Gustavo Araujo <gustavobicaaraujo92@gmail.com>
2025-04-18 15:53:36 -0300

    Adiciona componentes de navegação inferior (BottomNavigation)

---
+++ src/components/BottomNavigation.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './BottomNavigation.css';
+import homeIcon from '../assets/icons/home.png'; // ajuste conforme o caminho real
+
+const BottomNavigation = () => {
+  return (
+    <div className="bottom-nav">
+      <img src={homeIcon} alt="Home" className="nav-icon" />
+    </div>
+  );
+};
+
+export default BottomNavigation;
